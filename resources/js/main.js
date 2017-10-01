6f9ddc367417177061e096420bd54c2b5c8567fc
Piyush Ravi <mails4ipiyush@gmail.com>
2017-10-01 13:06:50 +0530

    add animations

--- resources/js/main.js
+++ resources/js/main.js
@@ -52,4 +52,28 @@ $(document).ready(function () {
                 }
             }
         });
-});
+
+        /* animations */
+
+        $('.js--wp-1').waypoint(function(direction) {
+             $('.js--wp-1').addClass('animated fadeIn');
+        }, { offset : '50%' });
+
+        $('.js--wp-2').waypoint(function(direction) {
+             $('.js--wp-2').addClass('animated fadeInUp');
+        }, { offset : '50%' });
+
+        $('.js--wp-3').waypoint(function(direction) {
+             $('.js--wp-3').addClass('animated zoomIn');
+        }, { offset : '50%' });
+
+        $('.js--wp-4').waypoint(function(direction) {
+             $('.js--wp-4').addClass('animated pulse');
+        }, { offset : '50%' });
+
+        setTimeout(function() {
+            $('.js--wp-5').addClass('animated pulse');
+        }, 14000);
+
+
+}); 
